9f5176e2b9391fe5b01d378261b4c8f22bd0e2b2
Diego Solis <disc3110@gmail.com>
2023-01-04 11:51:49 -0800

    Add dinamyc navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,47 @@
+const navHTML = `
+<ul class="nav nav-tabs">
+  <li class="nav-item dropdown">
+    <a class="nav-link" id="dropdown-btn" aria-current="page" href="#">Menu</a>
+    <div id="myDropdown" class="dropdown-content d-none">
+      <a href="#">Breakfast</a>
+      <a href="#">Saucers</a>
+      <a href="#">Desserts</a>
+      <a href="#">Beverages</a>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Locations</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Reservations</a>
+  </li>
+</ul>
+<p class="pt-3 pe-4">The Mexican</p>
+`;
+
+const displayNavBar = () => {
+  const navBAr = document.getElementById('nav-bar');
+
+  navBAr.innerHTML = navHTML;
+
+  const dropDownBTN = document.getElementById('dropdown-btn');
+  const myDropdown = document.getElementById('myDropdown');
+
+  dropDownBTN.addEventListener('click', () => {
+    myDropdown.classList.toggle('d-none');
+  });
+
+  window.addEventListener('click', (e) => {
+    if (!e.target.matches('#dropdown-btn')) {
+      if (!myDropdown.classList.contains('d-none')) myDropdown.classList.add('d-none');
+    }
+  });
+
+  // window.onclick = function (event) {
+  //       if (!event.target.matches('#dropdown-btn')) {
+  //     if (!myDropdown.classList.contains('d-none')) myDropdown.classList.add('d-none');
+  //   }
+  // };
+};
+
+export default displayNavBar;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+import displayNavBar from './components/navbar';
+
+displayNavBar();
