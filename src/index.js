4fe9c210fac94e4647f9ffa19640851aa2ab0354
Diego Solis <disc3110@gmail.com>
2023-01-04 19:44:14 -0800

    display menu from API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import displayNavBar from './components/navbar';
+import getmeals from './pages/menu';
 
 displayNavBar();
+getmeals();
